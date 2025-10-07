fbb0ee9ea5060dd6d266f9a61110b4fa9611f658
Lubomir Zelinsky <134857480+Lubober@users.noreply.github.com>
2025-10-07 13:18:48 +0100

    commit with vite instance

---
+++ portfolio-website/src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import AcademicWork from './pages/academic work.tsx'
+
+function App() {
+
+  return (
+    <>
+    <h1>Lubomir Zelinsky's Personal Website</h1>
+    <p>This is my app, inside it is :</p>
+    <AcademicWork/>
+    </>
+  )
+}
+
+export default App
---
+++ portfolio-website/src/pages/academic work.tsx
@@ -0,0 +1,9 @@
+
+const AcademicWork = () =>{
+    return(<>
+    <h1>My Academic Work</h1>
+    
+    </>)
+}
+
+export default AcademicWork
