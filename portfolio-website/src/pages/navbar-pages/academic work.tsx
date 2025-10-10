5943d28b0faf62d938f1c80c19c1ca5505721b72
Lubomir Zelinsky <134857480+Lubober@users.noreply.github.com>
2025-10-10 12:12:45 +0100

    aw

--- portfolio-website/src/pages/navbar-pages/academic work.tsx
+++ portfolio-website/src/pages/navbar-pages/academic work.tsx
@@ -1,8 +1,24 @@
 
 const AcademicWork = () =>{
+    const pdfUrl = import.meta.env.BASE_URL + 'files/cgen.pdf';
+    const pdfUrl1 = import.meta.env.BASE_URL + 'files/kernel.pdf';
     return(<>
+
     <h1>My Academic Work</h1>
-    
+    <div className="pdf-wrapper" style={{height: '80vh'}}>
+      <iframe
+        title="CV"
+        src={pdfUrl}
+        style={{ width: '100%', height: '100%', border: 0 }}
+      />
+    </div>
+    <div className="pdf-wrapper" style={{height: '80vh'}}>
+      <iframe
+        title="CV"
+        src={pdfUrl1}
+        style={{ width: '100%', height: '100%', border: 0 }}
+      />
+    </div>
     </>)
 }
 
